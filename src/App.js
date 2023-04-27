8e80ccdfe6336d0a1ebb1b54e88a13f69c387c52
Chidi Nwanneka <cnwanneka@gmail.com>
2023-04-27 23:34:30 +0100

    added code to the form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import FormValidator from './components/FormValidator'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <FormValidator />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
